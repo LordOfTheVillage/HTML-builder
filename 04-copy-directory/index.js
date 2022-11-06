b0d7fdf5235124431b03579e2e16fbd913faa314
LordOfTheVillage <maksim.kyl285@gmail.com>
2022-11-06 14:27:37 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require("fs/promises")
+const path = require("path")
+const directoryPath = path.join(__dirname, "/files")
+const directoryCopyPath = path.join(__dirname, "/files-copy")
 
+copyDir(directoryPath, directoryCopyPath)
+
+const cloneFiles = (main, copy, mainPath, copyPath) => {
+  const set = new Set([...main, ...copy])
+  for (const name of set) {
+    if ([...main].includes(name)) {
+      fs.copyFile(path.join(mainPath, name), path.join(copyPath, name))
+    } else {
+      fs.unlink(path.join(copyPath, name))
+    }
+  }
+}
+async function copyDir(mainPath, copyPath) {
+  try {
+    fs.mkdir(copyPath, { recursive: true })
+    const mainFiles = await fs.readdir(mainPath, { withFileTypes: false })
+    const copyFiles = await fs.readdir(copyPath, { withFileTypes: false })
+
+    cloneFiles(mainFiles, copyFiles, mainPath, copyPath)
+  } catch (error) {
+    console.error(error)
+  }
+}
