7daf90c9318c014b689c4887eb927e3c7f8b7f58
LordOfTheVillage <maksim.kyl285@gmail.com>
2022-11-06 12:00:51 +0300

    feat: add 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const fs = require("fs")
+const { stdin, stdout } = process
+const path = require("path")
+const pathName = path.join(__dirname, "/text.txt")
+
+stdout.write("Please, type tour text here:\n")
+
+stdin.on("data", (data) => {
+  const string = data.toString()
+  if (string.trim().toLowerCase() == "exit") process.exit()
+
+  fs.appendFile(pathName, string, (err) => {
+    if (err) throw err
+  })
+})
+
+process.on("SIGINT", () => process.exit())
+process.on("exit", () => stdout.write("Goodby!"))
